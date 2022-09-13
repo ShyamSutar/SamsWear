0e50b5ddd9bc7f50de8b726b8cdc21e853b066db
shyamsutar <shyamsutar629@gmail.com>
2022-09-13 21:23:05 +0530

    product/slug

--- pages/api/pincode.js
+++ pages/api/pincode.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json([422101, 422103, 234400,721302, 110002, 560017])
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,25 @@
-/* eslint-disable @next/next/no-img-element */
 import { useRouter } from 'next/router'
+import { useState } from 'react'
 
 const Post = () => {
   const router = useRouter()
   const { slug } = router.query
+  const [pin, setpin] = useState()
+  const [service, setservice] = useState()
+
+  const checkServiceability = async() => {
+    let pins = await fetch("http://localhost:3000/api/pincode");
+    let pinJson = await pins.json()
+    if(pinJson.includes(parseInt(pin))){
+      setservice(true)
+    }else{
+      setservice(false)
+    }
+  }
+
+  const onChangePin = (e) => {
+    setpin(e.target.value)
+  }
 
   return <>
     <section className="text-gray-600 body-font overflow-hidden">
@@ -13,8 +29,8 @@ const Post = () => {
 <img alt="ecommerce" className="h-96 rounded" src="https://m.media-amazon.com/images/I/81DKTXzkwnL._UY741_.jpg"/>
 </div>
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-        <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">The Catcher in the Rye</h1>
+        <h2 className="text-sm title-font text-gray-500 tracking-widest">SAMSWEAR</h2>
+        <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">Wear the code (XL/Blue)</h1>
         <div className="flex mb-4">
           <span className="flex items-center">
             <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
@@ -77,15 +93,23 @@ const Post = () => {
             </div>
           </div>
         </div>
-        <div className="flex">
-          <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
-          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
-          <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-            <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-            </svg>
-          </button>
+        <div className="flex space-x-2 justify-center md:justify-start">
+          <span className="title-font font-medium text-lg md:text-2xl text-gray-900">₹499.00</span>
+            <button className="flex text-white bg-indigo-500 border-0 py-2 px-2 text-base md:px-6 focus:outline-none hover:bg-indigo-600 rounded">Buy Now</button>
+            <button className="flex  text-white bg-indigo-500 border-0 py-2 px-2 md:px-6 text-base focus:outline-none hover:bg-indigo-600 rounded">Add to Cart</button>
+            <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+              </svg>
+            </button>
         </div>
+
+        <div className="pin mt-6 flex space-x-2 text-sm justify-center md:justify-start">
+          <input onChange={onChangePin} placeholder='Enter Your Pincode' type="text" className='px-2 border-2 border-gray-400 rounded-md'/>
+          <button onClick={checkServiceability} className='className="flex ml-auto text-white bg-indigo-500 border-0 rounded-md py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded"'>Check</button>
+        </div>
+        {!service && service != null && <div className="text-red-500 text-sm mt-3">Sorry! We do not deliver to this pincode yet</div>}
+        {service && service!=null && <div className="text-green-500 text-sm mt-3">This pincode is serviceable</div>}
       </div>
     </div>
   </div>
