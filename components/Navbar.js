e8443a1299b86df88fad5c381c4e671b156cd810
shyamsutar <shyamsutar629@gmail.com>
2022-09-14 14:51:47 +0530

    making fuctional cart

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,8 @@ import React, { useRef } from "react";
 import { AiOutlineShoppingCart, AiFillCloseCircle, AiFillMinusCircle, AiFillPlusCircle } from "react-icons/ai";
 import { BsFillBagCheckFill } from "react-icons/bs";
 
-const Navbar2 = () => {
+const Navbar2 = ({cart, addToCart, removeFromCart, clearCart, subTotal}) => {
+
   const toggleCart = () => {
     if (ref.current.classList.contains("translate-x-full")) {
       ref.current.classList.remove("translate-x-full");
@@ -54,58 +55,20 @@ const Navbar2 = () => {
       <span  className="absolute top-2 right-2 cursor-pointer text-2xl text-indigo-500"><AiFillCloseCircle onClick={toggleCart}/></span>
       
       <ul className="list-decimal font-semibold">
-        <li>
+       {Object.keys(cart).length ==  0 && <div className="flex justify-center text-base">
+         Your cart is empty
+       </div>}
+        {Object.keys(cart).map((k)=>{return <li key={k}>
           <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
+          <div className="w-2/3 font-semibold">{cart[k].name}</div>
+          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle onClick={()=>{removeFromCart(k, 1, cart[k].name, cart[k].size, cart[k].variant)}} className="cursor-pointer  text-indigo-500"/> <span className="mx-2 text-sm">{cart[k].qty}</span> <AiFillPlusCircle onClick={()=>{addToCart(k, 1, cart[k].name, cart[k].size, cart[k].variant)}} className="cursor-pointer text-indigo-500"/></div>
           </div>
-        </li>
-
-        <li>
-          <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
-          </div>
-        </li>
-
-        <li>
-          <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
-          </div>
-        </li>
-
-        <li>
-          <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
-          </div>
-        </li>
-
-        <li>
-          <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
-          </div>
-        </li>
-
-        <li>
-          <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
-          </div>
-        </li>
-        <li>
-          <div className="item flex my-5">
-          <div className="w-2/3 font-semibold">Tshirts - Wear the Code</div>
-          <div className="flex items-center justify-center w-1/3 font-semibold text-lg"><AiFillMinusCircle  className="cursor-pointer text-indigo-500"/> <span className="mx-2 text-sm">1</span> <AiFillPlusCircle className="cursor-pointer text-indigo-500"/></div>
-          </div>
-        </li>
+        </li>})}
        
       </ul>
           <div className="flex">
             <button className="flex mr-2 mt-2 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-base items-center"><BsFillBagCheckFill className="mx-2"/> Checkout</button>
-            <button className="flex mx-2 mt-2 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-base "> Clear Cart</button>
+            <button onChange={clearCart} className="flex mx-2 mt-2 text-white bg-indigo-500 border-0 py-1 px-2 focus:outline-none hover:bg-indigo-600 rounded text-base "> Clear Cart</button>
           </div>
        </div>
     </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,70 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  const [cart, setCart] = useState({});
+  const [subTotal, setSubTotal] = useState(0)
+
+  useEffect(() => {
+    if(localStorage.getItem("cart")){
+      try {
+        setCart(JSON.parse(localStorage.getItem('cart')));
+      } catch (error) {
+        console.error(error);
+        localStorage.clear()
+      }
+    }
+  }, [])
+  
+
+  const saveCart = (myCart) =>{
+    localStorage.setItem('cart', JSON.stringify(myCart));
+    let subt = subTotal;
+    let keys = Object.keys(myCart);
+    for (let i = 0; i < keys.length; i++) {
+      subt += myCart[keys[i]].price * myCart[keys[i]].qty;
+      
+    }
+    setSubTotal(subt);
+  }
+
+  const addToCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = cart;
+    if(itemCode in cart){
+      newCart[itemCode].qty = cart[itemCode].qty + qty;
+    }else{
+      newCart[itemCode] = {qty: 1, price, name, size, variant}
+    }
+    setCart(newCart);
+    saveCart(newCart);
+  }
+
+  const clearCart = () =>{
+    setCart({});
+    saveCart({});
+    console.log("cart has been cleared");
+  }
+
+  const removeFromCart = (itemCode, qty, price, name, size, variant) => {
+    let newCart = JSON.parse(JSON.stringify(cart));
+    if(itemCode in cart){
+      newCart[itemCode].qty = cart[itemCode].qty - qty;
+    }
+    if(newCart[itemCode].qty <= 0){
+      delete newCart[itemCode];
+    }
+
+    setCart(newCart)
+    saveCart(newCart);
+  }
+
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
+      <Navbar cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart = {clearCart} subTotal={subTotal} />
+      <Component cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} clearCart = {clearCart} subTotal={subTotal} {...pageProps} />
       <Footer />
     </>
   );
